e8b97d74fc642f4a71fb084e92689222d1f5cf3d
Heimdim <heimdim@gmail.com>
2020-01-16 01:55:21 +0300

    Add project files.

---
+++ Tabmaker.Model/Context/UserContext.cs
@@ -0,0 +1,18 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabmaker.Server.Model;
+
+namespace Tabmaker.Server.Context
+{
+    class UserContext : DbContext
+    {
+        public UserContext() : base("tabmaker_db")
+        { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Tabmaker.Model/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tabmaker.Server.Context;
+using Tabmaker.Server.Model;
+
+namespace Tabmaker.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
